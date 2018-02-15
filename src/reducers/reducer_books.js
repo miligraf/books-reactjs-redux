c38b835627be891620f90f27d0daee16d1705b10
Milton Andrade <miligraf@gmail.com>
2018-02-15 08:23:43 -0500

    Use Redux to maintain app state

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'JavaScript', pages: 101},
+		{ title: 'PHP', pages: 39 },
+		{ title: 'Python', pages: 85 },
+		{ title: 'Ruby', pages: 1 }
+	]
+}
